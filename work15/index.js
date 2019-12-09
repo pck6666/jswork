62d7815f9163e97b2ed4bb7f361e0671bc04cb5a
pck6666 <1466923208@qq.com>
2019-12-09 15:54:24 +0800

    work15

---
+++ work15/index.js
@@ -0,0 +1,21 @@
+ShopCart('cart', [{
+    name:'真三国无双5(游戏光盘)',
+    price:69.0,
+    num:2
+},
+{
+    name:'崩坏3rd((氪金键)',
+    price:648.0,
+    num:2
+},
+{
+    name:'王者荣耀(皮肤购买)',
+    price:328.0,
+    num:3
+},
+{
+    name:'NBA 2K online(球星购买)',
+    price:168.0,
+    num:3
+}
+]);
